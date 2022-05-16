9145407b7ceefc3191bb6158f23409513e0de6fd
Rafael Oliveira <raf100guy@gmail.com>
2022-05-16 20:29:53 -0300

    Reactive Mongo + Docker

--- cliente-api/src/main/java/com/letscode/cliente/repository/ClienteRepository.java
+++ cliente-api/src/main/java/com/letscode/cliente/repository/ClienteRepository.java
@@ -1,9 +1,10 @@
 package com.letscode.cliente.repository;
 
 import com.letscode.cliente.dto.ClienteDTO;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends MongoRepository<ClienteDTO, String> {
+public interface ClienteRepository extends ReactiveMongoRepository<ClienteDTO, String> {
+
 }
